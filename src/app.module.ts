c3508155e990bf70c7d46e1c5493d64dddf1f3c5
Edgar Cortez-Castellano <eac0001@dcccd.edu>
2024-08-21 13:55:31 -0500

    Attempting to refactor Tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
+import { TasksModule } from './tasks-old/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
