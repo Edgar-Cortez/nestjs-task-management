0e4a94d1a7f4464f2a59cfecb2bbe92a67c5ff2f
Edgar Cortez-Castellano <eac0001@dcccd.edu>
2024-08-29 15:53:41 -0500

    removed comments

--- src/tasks/dto/tasks-status.enum.ts
+++ src/tasks/dto/tasks-status.enum.ts
@@ -1,11 +1,3 @@
-// THIS WAS BEFORE USING POSTGRESQL, WE ARE USING task.entity.ts
-// export interface Task {
-//   id: string;
-//   title: string;
-//   description: string;
-//   status: TaskStatus;
-// }
-
 export enum TaskStatus {
   OPEN = 'OPEN',
   IN_PROGRESS = 'IN_PROGRESS',
